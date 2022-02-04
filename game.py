8c423c92ca3a1f5dc2cd5a2614cfcbe70ec9f3d2
annabellealbert95 <80716097+annabellealbert95@users.noreply.github.com>
2022-02-03 20:37:42 -0500

    class work
    intro python code done in class

--- game.py
+++ game.py
@@ -1,7 +1,30 @@
 
-
-
 #
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+
+
+
+#Requirements 
+
+#1) Processing User inputs 
+
+user_choice = input("Please choose one of: 'rock', 'paper', 'scissors': ")
+
+print("User Chose:", user_choice)
+
+#2) Validating user inputs
+
+#3) Simulating computer selection 
+
+import random
+
+options = ("rock", "paper", "scissors")
+computer_choice = random.choice(options)
+
+print("Computer Chose:", computer_choice)
+
+#4) determining the winner 
+
+#5) displaying results 
