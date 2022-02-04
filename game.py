032b8a3c766064745bd290ce86df203f90220642
annabellealbert95 <80716097+annabellealbert95@users.noreply.github.com>
2022-02-03 20:56:33 -0500

    determining the winner
    done during class as well ... determining the winner of R,P,S

--- game.py
+++ game.py
@@ -2,12 +2,9 @@
 #
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
-#
-
 
 
 #Requirements 
-
 #1) Processing User inputs 
 
 user_choice = input("Please choose one of: 'rock', 'paper', 'scissors': ")
@@ -26,5 +23,25 @@ computer_choice = random.choice(options)
 print("Computer Chose:", computer_choice)
 
 #4) determining the winner 
+# using an if statement to choose the winner
+# got some help with this during class time
+
+if user_choice == computer_choice:
+    print("Both players played", user_choice, "It is a tie :/")
+elif user_choice == "paper":
+    if computer_choice == "rock":
+        print("Paper covers rock. You won :) ")
+    else:
+        print("Scissors cuts paper. You lost :(")
+elif user_choice == "scissors":
+    if computer_choice == "paper":
+        print("Scissors cuts paper. You won :)")
+    else:
+        print("Rock crushes scissors. You lost :(")
+elif user_choice == "rock":
+    if computer_choice == "scissors":
+        print("Rock crushes scissors. You won :)")
+    else:
+        print("Paper covers rock. You lost :(")
 
 #5) displaying results 
